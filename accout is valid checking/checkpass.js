d4cab261e2bf75e92726950d477336c54de0c5cf
Thienan1092005 <ogyminecraft497@gmail.com>
2024-03-09 11:58:16 +0700

    vcl

--- accout is valid checking/checkpass.js
+++ accout is valid checking/checkpass.js
@@ -1,6 +1,7 @@
 function getUserInput() {
   let userName = document.getElementById("userNameInput").value;
   let password = document.getElementById("passwordInput").value;
+  let loginDone = false;
   if (!userName || !password) {
     alert(
       "vui long nhap tai khoan va mat khau truoc khi dang nhap xin cam on "
@@ -8,6 +9,7 @@ function getUserInput() {
   } else if (userName == "admin" && password == "123") {
     alert("dang nhap thanh cong ");
     window.location.href = "./index2.html";
+    loginDone = true;
   } else {
     alert("tai khoan hoac mat khau khong dung vui long thu  lai ");
   }
