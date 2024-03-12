3cbb5ac61eec24279ed67a81a93a6bb3315bf363
Thienan1092005 <ogyminecraft497@gmail.com>
2024-03-12 13:30:48 +0700

    isdone

--- accout is valid checking/checkpass.js
+++ accout is valid checking/checkpass.js
@@ -1,11 +1,7 @@
+//loginCheckm
 function getUserInput() {
   let userName = document.getElementById("userNameInput").value;
   let password = document.getElementById("passwordInput").value;
-  let loginDone = false;
-  const play = document.querySelector("#play");
-  const song = document.querySelector("#song");
-  const plate = document.querySelector("#plate");
-  let isPlay = false;
   if (!userName || !password) {
     alert(
       "vui long nhap tai khoan va mat khau truoc khi dang nhap xin cam on "
@@ -18,24 +14,126 @@ function getUserInput() {
     alert("tai khoan hoac mat khau khong dung vui long thu  lai ");
   }
 }
-
-play.addEventListener("click", () => {
-  if (isPlay == false) {
-    song.play();
-    plate.style.animation = "rotateTurntable 5s linear infinite";
-    play.setAttribute("name", "pause-circle-outline");
-    console.log("play a song ");
-    isPlay = true;
+//song contriol
+const song = document.getElementById("song");
+const playBtn = document.querySelector(".player-inner");
+const nextBtn = document.querySelector(".play-forward");
+const prevBtn = document.querySelector(".play-back");
+const durationTime = document.querySelector(".duration");
+const remainingTime = document.querySelector(".remaining");
+const rangeBar = document.querySelector(".range");
+const musicName = document.querySelector(".music-name");
+const musicThumbnail = document.querySelector(".music-thumb");
+const musicImage = document.querySelector(".music-thumb img");
+const playRepeat = document.querySelector(".play-repeat");
+let isPlaying = true;
+let indexSong = 0;
+let isRepeat = false;
+const musics = [
+  {
+    id: 1,
+    title: "NguoiDien",
+    file: "NguoiDien.mp3",
+    image:
+      "https://images.unsplash.com/photo-1614624532983-4ce03382d63d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80",
+  },
+  {
+    id: 2,
+    title: "DaiMinhTinh",
+    file: "DaiMinhTinh.mp3",
+    image:
+      "https://images.unsplash.com/photo-1616763355548-1b606f439f86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  },
+];
+let timer;
+let repeatCount = 0;
+playRepeat.addEventListener("click", function () {
+  if (isRepeat) {
+    isRepeat = false;
+    playRepeat.removeAttribute("style");
   } else {
-    song.pause();
-    plate.style.animation = "none";
-    play.setAttribute("name", "play-circle-outline");
-    console.log("!play a song ");
-    isPlay = false;
+    isRepeat = true;
+    playRepeat.style.color = "#ffb86c";
   }
 });
-song.addEventListener("ended", () => {
-  plate.style.animation = "none";
-  play.setAttribute("name", "play-circle-outline");
-  isPlay = false;
+nextBtn.addEventListener("click", function () {
+  changeSong(1);
 });
+prevBtn.addEventListener("click", function () {
+  changeSong(-1);
+});
+song.addEventListener("ended", handleEndedSong);
+function handleEndedSong() {
+  repeatCount++;
+  if (isRepeat && repeatCount === 1) {
+    isPlaying = true;
+    playPause();
+  } else {
+    changeSong(1);
+  }
+}
+function changeSong(dir) {
+  if (dir === 1) {
+    // next song
+    indexSong++;
+    if (indexSong >= musics.length) {
+      indexSong = 0;
+    }
+    isPlaying = true;
+  } else if (dir === -1) {
+    // prev song
+    indexSong--;
+    if (indexSong < 0) {
+      indexSong = musics.length - 1;
+    }
+    isPlaying = true;
+  }
+  init(indexSong);
+  // song.setAttribute("src", `./music/${musics[indexSong].file}`);
+  playPause();
+}
+playBtn.addEventListener("click", playPause);
+function playPause() {
+  if (isPlaying) {
+    musicThumbnail.classList.add("is-playing");
+    song.play();
+    playBtn.innerHTML = `<ion-icon name="pause-circle"></ion-icon>`;
+    isPlaying = false;
+    timer = setInterval(displayTimer, 500);
+  } else {
+    musicThumbnail.classList.remove("is-playing");
+    song.pause();
+    playBtn.innerHTML = `<ion-icon name="play"></ion-icon>`;
+    isPlaying = true;
+    clearInterval(timer);
+  }
+}
+function displayTimer() {
+  const { duration, currentTime } = song;
+  rangeBar.max = duration;
+  rangeBar.value = currentTime;
+  remainingTime.textContent = formatTimer(currentTime);
+  if (!duration) {
+    durationTime.textContent = "00:00";
+  } else {
+    durationTime.textContent = formatTimer(duration);
+  }
+}
+function formatTimer(number) {
+  const minutes = Math.floor(number / 60);
+  const seconds = Math.floor(number - minutes * 60);
+  return `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+}
+rangeBar.addEventListener("change", handleChangeBar);
+function handleChangeBar() {
+  song.currentTime = rangeBar.value;
+}
+function init(indexSong) {
+  song.setAttribute("src", `./MEDIA/${musics[indexSong].file}`);
+  musicImage.setAttribute("src", musics[indexSong].image);
+  musicName.textContent = musics[indexSong].title;
+}
+displayTimer();
+init(indexSong);
